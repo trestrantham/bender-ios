c2ba844a3fdd88caf903df018e3f28454f4b89df
Tres Trantham <tres@trestrantham.com>
2013-02-26 01:04:52 -0500

    Added new user

---
+++ app/controllers/add_user_controller.rb
@@ -0,0 +1,75 @@
+class AddUserController < Formotion::FormController
+	attr_accessor :parent_controller
+  @parent_controller = nil
+
+	def init
+		form = Formotion::Form.new({
+			sections: [{
+				title: "New User",
+				rows: [{
+					title: "Name",
+					key: :name,
+					placeholder: "Foo",
+					type: :string,
+					auto_correction: :no
+				}]
+			}, {
+				rows: [{
+					title: "Create",
+					type: :submit,
+				}]
+			}]
+		})
+
+		form.on_submit do
+			self.save
+		end
+		super.initWithForm(form)
+	end
+
+	def viewDidLoad
+		super
+		self.title = "Create User"
+
+		self.navigationItem.rightBarButtonItem ||= UIBarButtonItem.alloc.initWithBarButtonSystemItem(
+																									UIBarButtonSystemItemSave, 
+																									target:self, 
+																									action:"save")
+
+		self.navigationItem.leftBarButtonItem ||= UIBarButtonItem.alloc.initWithBarButtonSystemItem(
+																									UIBarButtonSystemItemCancel, 
+																									target:self, 
+																									action:"cancel")
+	end
+
+	def viewDidDisappear(animated)
+		super(animated)
+
+		@table_data = nil
+	end
+
+	def save
+		puts ""
+		@form_data = self.form.render
+		puts "AddNewUserController > save > @form_data: #{@form_data}"
+
+		user = {user: {name: @form_data[:name]}} if @form_data.has_key?(:name)
+
+		# Set spinner
+		# AppHelper.parse_api(:post, "/users.json", {payload: user}) do |response|
+		BW::HTTP.post("#{App::Persistence[:api_url]}/users.json", {payload: user}) do |response|
+			if response.ok?
+				@parent_controller.load_data if @parent_controller
+				self.form.sections[0].rows[0].value = "" # Clear user's name in case modal is called more than once
+				self.dismissModalViewControllerAnimated(true)
+			else
+				App.alert("Invalid User")
+			end
+		end
+	end
+
+	def cancel
+		self.form.sections[0].rows[0].value = "" # Clear user's name in case modal is called more than once
+		self.dismissModalViewControllerAnimated(true)
+	end
+end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,45 +16,48 @@ class UserController < UIViewController
 		self
 	end
 
-  def viewDidLoad
-    super
-    self.title = "Users"
-    @users = []
+	def viewDidLoad
+		super
+		self.title = "Users"
+		@users = []
 
-    @table ||= UITableView.alloc.initWithFrame(self.view.bounds)
+		@table ||= UITableView.alloc.initWithFrame(self.view.bounds)
 		@table.dataSource = self
 		@table.delegate = self
 		@table.addPullToRefreshWithActionHandler( Proc.new { load_data } )
 		self.view.addSubview @table
 
+		self.navigationItem.rightBarButtonItem ||= UIBarButtonItem.alloc.initWithBarButtonSystemItem(
+																							UIBarButtonSystemItemAdd, 
+																							target:self, 
+																							action:"new_user")
+
 		load_data
-  end
+	end
 
-  def viewDidDisappear(animated)
-  	super(animated)
+	def viewDidDisappear(animated)
+		super(animated)
 
-  	@pour_active = true
-  end
+		@pour_active = true
+	end
 
 	def load_data
 		return if App::Persistence[:api_url].blank?
 
-		if !AppHelper.valid_url?(App::Persistence[:api_url])
-			App.alert("Invalid URL.")
-			return
-		end
+		AppHelper.parse_api(:get, "/users.json") do |response|
+			json = p response.body.to_str
+			@users = BW::JSON.parse json
 
-		BW::HTTP.get("#{App::Persistence[:api_url]}/users.json") do |response|
-			if response.ok?
-				json = p response.body.to_str
-				@users = BW::JSON.parse json
-
-				@table.reloadData
-				@table.pullToRefreshView.stopAnimating
-			else
-				App.alert("Server cannot be reached.")
-			end
+			@table.reloadData
+			@table.pullToRefreshView.stopAnimating
 		end
+	end
+
+	def new_user
+		@user ||= AddUserController.new
+		@user.parent_controller = self
+		@user_navigation = UINavigationController.alloc.initWithRootViewController(@user)
+		self.presentModalViewController(@user_navigation, animated:true)
 	end
 
 	def numberOfSectionsInTableView(tableView)
@@ -65,16 +68,16 @@ class UserController < UIViewController
 		@users.size
 	end
 
-  def tableView(tableView, cellForRowAtIndexPath: indexPath)
-    @reuseIdentifier ||= "CELL_IDENTIFIER"
+	def tableView(tableView, cellForRowAtIndexPath: indexPath)
+		@reuseIdentifier ||= "CELL_IDENTIFIER"
 
-    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
-      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
-    end
+		cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+			UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+		end
 
 		cell.textLabel.text = @users[indexPath.row][:name]
-    cell
-  end
+		cell
+	end
 
 	def tableView(tableView, didSelectRowAtIndexPath:indexPath)
 		user = @users[indexPath.row]
@@ -84,30 +87,15 @@ class UserController < UIViewController
 		self.navigationController.pushViewController(pour_controller, animated: true)
 	end
 
-	# TODO(Tres): refactor this to it's own module
 	def get_beer_tap(beer_tap = {})
 		puts ''
 		puts "UserController > get_beer_tap"
 
-		if App::Persistence[:api_url].blank?
-			App.alert("API URL is required!")
-			return
-		end
-
-		if !AppHelper.valid_url?(App::Persistence[:api_url])
-			App.alert("Invalid URL.")
-			return
-		end
-
-		BW::HTTP.get("#{App::Persistence[:api_url]}/admin/beer_taps/#{beer_tap[:id]}.json") do |response|
-			if response.ok?
-				puts "UserController > get_beer_tap > GOT BEER TAP using #{App::Persistence[:api_url]}/admin/beer_taps/#{beer_tap[:id]}.json"
-				json = p response.body.to_str
-				@beer_tap = BW::JSON.parse json
-				@beer_tap.symbolize_keys!
-			else
-				App.alert("Server cannot be reached.")
-			end
+		AppHelper.parse_api(:get, "/admin/beer_taps/#{beer_tap[:id]}.json") do |response|
+			puts "UserController > get_beer_tap > GOT BEER TAP using #{App::Persistence[:api_url]}/admin/beer_taps/#{beer_tap[:id]}.json"
+			json = p response.body.to_str
+			@beer_tap = BW::JSON.parse json
+			@beer_tap.symbolize_keys!
 		end
 	end
 end
