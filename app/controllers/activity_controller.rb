edbfaab82dc353c80c804a510d51809dfb6df341
Tres Trantham <tres@trestrantham.com>
2013-03-07 23:26:56 -0500

    Add style; Change activity to UIViewController

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,9 +1,18 @@
-class ActivityController < UITableViewController
+class ActivityController < UIViewController
   def viewDidLoad
     super
+
     self.title = "Recent Activity"
-    @activities = [{name:"activity"}, {name:"foo"}, {name:"bar"}, {name:"baz"}]
-    #tableView.addPullToRefreshWithActionHandler( Proc.new { load_data } )
+    @activities = [{name:"Tres T. poured 8.1 oz of Homebrew"}, {name:"David G. poured 6.3 oz of Homebrew"}, {name:"Tim B. poured 9.4 oz of Homebrew"}]
+
+    @table_view = UITableView.alloc.initWithFrame([[0, 0], [388, 211]], style: UITableViewStyleGrouped)
+    @table_view.delegate = self
+    @table_view.dataSource = self
+    @table_view.separatorStyle = UITableViewCellSeparatorStyleNone
+    @table_view.backgroundView = nil
+
+    self.view << @table_view
+
     #load_data
   end
 
@@ -11,34 +20,37 @@ class ActivityController < UITableViewController
     return if App::Persistence[:api_url].blank?
 
     AppHelper.parse_api(:get, "/activity/recent.json") do |response|
-      json = p response.body.to_str
-      @activities = BW::JSON.parse json
+      @activities = BW::JSON.parse response.body
       
-      @table.reloadData
-      @table.pullToRefreshView.stopAnimating
+      @table_view.reloadData
     end
   end
 
-  def numberOfSectionsInTableView(tableView)
+  def numberOfSectionsInTableView(table_view)
     1
   end
 
-  def tableView(tableView, numberOfRowsInSection: section)
+  def tableView(table_view, numberOfRowsInSection: section)
     @activities.size
   end
 
-  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+  def tableView(table_view, titleForHeaderInSection: section)
+    "Recent Activity"
+  end
+
+  def tableView(table_view, cellForRowAtIndexPath: index_path)
     @reuseIdentifier ||= "CELL_IDENTIFIER"
 
-    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+    cell = table_view.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
       UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
     end
 
-    cell.textLabel.text = @activities[indexPath.row][:name]
+    cell.textLabel.text = @activities[index_path.row][:name]
+    cell.backgroundColor = :clear.uicolor
     cell
   end
 
-  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
-    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+  def tableView(table_view, didSelectRowAtIndexPath:index_path)
+    table_view.deselectRowAtIndexPath(index_path, animated: true)
   end
 end
--- app/controllers/context_controller.rb
+++ app/controllers/context_controller.rb
@@ -1,25 +1,28 @@
 class ContextController < UIViewController
-  SCROLL_PAGE_WIDTH = 788
+  SCROLL_PAGE_WIDTH = 768
 
   def viewDidLoad
-    @scroll_view = UIScrollView.alloc.initWithFrame([[0, 0], [768, 251]])
+    @scroll_view = UIScrollView.alloc.initWithFrame([[0, 0], [728, 211]])
+    @scroll_view.contentSize = [728 * 2, 211]
+    @scroll_view.backgroundColor = "#333".uicolor
+    @scroll_view.scrollsToTop = false
+    @scroll_view.bounces = false
     @scroll_view.pagingEnabled = true
-    @scroll_view.contentSize = [768 * 2, 251]
     @scroll_view.showsHorizontalScrollIndicator = false
     @scroll_view.showsVerticalScrollIndicator = false
-    @scroll_view.scrollsToTop = false
-    @scroll_view.backgroundColor = UIColor.lightGrayColor
-    self.view.addSubview(@scroll_view)
 
-    @activity_controller = ActivityController.alloc.initWithStyle(UITableViewStyleGrouped, frame: [[0,0],[0,0]])
-    @activity_controller.view.frame = [[0, 0], [768, 251]]
-    @scroll_view.addSubview(@activity_controller.view)
+    @scroll_view.layer.masksToBounds = true
+    @scroll_view.layer.borderColor = :black.uicolor.CGColor
+    @scroll_view.layer.borderWidth = 1
+
+    @activity_controller = ActivityController.new #alloc.initWithStyle(UITableViewStyleGrouped, frame: [[0,0],[0,0]])
+    @activity_controller.view.frame = [[0, 0], [728, 211]]
+    @scroll_view << @activity_controller.view
 
     @metrics_controller = MetricsController.alloc.initWithStyle(UITableViewStyleGrouped, frame: [[0,0],[0,0]])
-    @metrics_controller.view.frame = [[768, 0], [768, 251]]
-    @scroll_view.addSubview(@metrics_controller.view)
-
-    #headerSeperatorLine = ShadowView.alloc.initWithFrame([[-5, 0], [778, 20]], withHeight: 1, withShadowSize: 10, withShadowBlur: 10, withColor: UIColor.#darkGrayColor)
-    #self.view.addSubview(headerSeperatorLine)
+    @metrics_controller.view.frame = [[728, 0], [728, 211]]
+    @scroll_view << @metrics_controller.view
+    
+    self.view << @scroll_view
   end
 end
