89d9d40ad1bd9de1ef1718009754a5359b73340a
Tres Trantham <tres@trestrantham.com>
2013-08-28 15:02:53 -0400

    Removed motion-support; Copied support for blank?

--- app/app_helper.rb
+++ app/app_helper.rb
@@ -133,3 +133,56 @@ class NSDate
     relative_string
   end
 end
+
+
+class Object
+  def blank?
+    respond_to?(:empty?) ? empty? : !self
+  end
+
+  def present?
+    !blank?
+  end
+
+  def presence
+    self if present?
+  end
+end
+
+class NilClass
+  def blank?
+    true
+  end
+end
+
+class FalseClass
+  def blank?
+    true
+  end
+end
+
+class TrueClass
+  def blank?
+    false
+  end
+end
+
+class Array
+  alias_method :blank?, :empty?
+end
+
+class Hash
+  alias_method :blank?, :empty?
+end
+
+class String
+  def blank?
+    self !~ /[^[:space:]]/
+  end
+end
+
+class Numeric #:nodoc:
+  def blank?
+    false
+  end
+end
